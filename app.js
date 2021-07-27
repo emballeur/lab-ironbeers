502b954a1d4b93991a1faa55f14da3c3f10ca4c3
sina tams <sina.tams@web.de>
2021-07-27 19:15:08 +0200

    sth btw it 3 and 4 semiworking

--- app.js
+++ app.js
@@ -10,16 +10,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (request, response, next) => {
+  response.render('home');
+});
+
+app.get('/beers', (request, response, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => response.render('beers', { beers }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer ', (request, response, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
